9f8b8fcd15d45e5045bfce0200f794e6b36a09fd
John Duffell <john.duffell@guardian.co.uk>
2015-02-24 12:36:49 +0000

    try to fix travis build

--- static/src/javascripts/projects/common/utils/cookies.js
+++ static/src/javascripts/projects/common/utils/cookies.js
@@ -30,13 +30,15 @@ define([
     function add(name, value, daysToLive) {
 
         var expires = new Date();
+        console.log("epoch", expires, expires.toUTCString(), expires.toString());
 
         if (daysToLive) {
             expires.setDate(expires.getDate() + daysToLive);
         } else {
             expires.setUTCMonth(expires.getUTCMonth() + 5);
-            expires.setDate(1);
+            expires.setUTCDate(1);
         }
+        console.log("modified", expires, expires.toUTCString(), expires.toString());
 
         getDocument().cookie =
             name + '=' + value + '; path=/; expires=' + expires.toUTCString() + ';' + getDomainAttribute();
