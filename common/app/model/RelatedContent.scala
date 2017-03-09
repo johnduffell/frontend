491baed4094160fdb97f790e8fbfe86e7a412b44
John Duffell <john.duffell@guardian.co.uk>
2017-03-09 17:38:02 +0000

    stop it using any story package that may be there

--- common/app/model/RelatedContent.scala
+++ common/app/model/RelatedContent.scala
@@ -22,25 +22,6 @@ case class RelatedContent (items: Seq[RelatedContentItem]) {
 
 object StoryPackages {
   def apply(parent: ContentType, response: ItemResponse): RelatedContent = {
-
-    val storyPackagesContent: Seq[ApiContent] = response.packages.map { packages =>
-      val allContentsPerPackage: Seq[Seq[ApiContent]] = packages.map(_.articles.map(_.content))
-      if(packages.size > 1) { //intermix packages only if more than one
-        allContentsPerPackage
-         .flatMap(_.zipWithIndex) // zip content with its position
-         .groupBy(_._1.id).map(_._2.head).toSeq // remove duplicates (Note: not using `distinct` here that would require
-                                                // hashing/comparing the whole ApiContent object but instead grouping based on `id`
-                                                // and then collecting the first element of each group of duplicates)
-         .sortBy(_._2).map(_._1)  // sort by position and extract content
-      } else {
-        allContentsPerPackage.flatten
-      }
-    }.getOrElse(List.empty)
-
-    val items = storyPackagesContent.map { item =>
-      val frontendContent = Content(item)
-      RelatedContentItem(frontendContent, FaciaContentConvert.contentToFaciaContent(item))
-    }
-    RelatedContent(items.filterNot(_.content.metadata.id == parent.metadata.id))
+    RelatedContent(Nil: Seq[RelatedContentItem])
   }
 }
